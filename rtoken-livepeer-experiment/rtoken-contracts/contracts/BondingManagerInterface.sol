c7560878a61375660d247c223555c8aa7505df44
William Griffiths <willjgriff@gmail.com>
2019-12-03 12:01:02 +0000

    Initial expected minimal code for LivepeerAllocationStrategy.

--- rtoken-livepeer-experiment/rtoken-contracts/contracts/BondingManagerInterface.sol
+++ rtoken-livepeer-experiment/rtoken-contracts/contracts/BondingManagerInterface.sol
@@ -2,7 +2,18 @@ pragma solidity ^0.5.1;
 
 contract BondingManagerInterface {
 
-    constructor() public {
+    function getDelegator(address _delegator) public view returns
+    (
+        uint256 bondedAmount,
+        uint256 fees,
+        address delegateAddress,
+        uint256 delegatedAmount,
+        uint256 startRound,
+        uint256 lastClaimRound,
+        uint256 nextUnbondingLockId
+    );
 
-    }
+    function bond(uint256 _amount, address _to) external;
+
+    function unbond(uint256 _amount) external;
 }
--- rtoken-livepeer-experiment/rtoken-contracts/contracts/LivepeerAllocationStrategy.sol
+++ rtoken-livepeer-experiment/rtoken-contracts/contracts/LivepeerAllocationStrategy.sol
@@ -2,15 +2,23 @@ pragma solidity >=0.5.10 <0.6.0;
 
 import {IAllocationStrategy} from "./IAllocationStrategy.sol";
 import {IERC20} from "openzeppelin-solidity/contracts/token/ERC20/IERC20.sol";
+import {BondingManagerInterface} from "./BondingManagerInterface.sol";
 
 contract LivepeerAllocationStrategy is IAllocationStrategy {
 
-    uint256 exchangeRateStored = 1;
+    uint256 private currentExchangeRate = 1;
+    uint256 private previousDelegatedTotal = 0;
 
     IERC20 livepeerToken;
+    BondingManagerInterface bondingManager;
+    address stakeCapitalPoolDelegator;
+    address stakeCapitalTranscoder;
 
-    constructor(IERC20 _livepeerToken) public {
+    constructor(IERC20 _livepeerToken, BondingManagerInterface _bondingManager, address _stakeCapitalPoolDelegator, address _stakeCapitalTranscoder) public {
         livepeerToken = _livepeerToken;
+        bondingManager = _bondingManager;
+        stakeCapitalPoolDelegator = _stakeCapitalPoolDelegator;
+        stakeCapitalTranscoder = _stakeCapitalTranscoder;
     }
 
     function underlying() external view returns (address) {
@@ -18,19 +26,24 @@ contract LivepeerAllocationStrategy is IAllocationStrategy {
     }
 
     function exchangeRateStored() external view returns (uint256) {
-        return exchangeRateStored;
+        return currentExchangeRate;
     }
 
     function accrueInterest() external returns (bool) {
-        exchangeRateStored = exchangeRateStored + exchangeRateStored * (new delegated total / (previous delegated total + minted since previous reward - redeemed since previous reward) - 1);
+        uint256 currentDelegatedTotal;
+        (,,, currentDelegatedTotal,,,) = bondingManager.getDelegator(stakeCapitalPoolDelegator);
+        currentExchangeRate = currentExchangeRate + currentExchangeRate * (currentDelegatedTotal / previousDelegatedTotal - 1); // + minted since previous reward - redeemed since previous reward) - 1);
+        previousDelegatedTotal = currentDelegatedTotal;
         return true;
     }
 
     function investUnderlying(uint256 investAmount) external returns (uint256) {
-        return 1;
+        bondingManager.bond(investAmount, stakeCapitalTranscoder);
+        return investAmount / currentExchangeRate;
     }
 
     function redeemUnderlying(uint256 redeemAmount) external returns (uint256) {
-        return 1;
+        bondingManager.unbond(redeemAmount);
+        return redeemAmount / currentExchangeRate;
     }
 }
