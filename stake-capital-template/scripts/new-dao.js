04f3b28f83c7b4d00aa4bfad927bd04508df5c72
William Griffiths <willjgriff@gmail.com>
2019-12-10 12:54:10 +0000

    Fixed issue with non-displaying apps.

--- stake-capital-template/scripts/new-dao.js
+++ stake-capital-template/scripts/new-dao.js
@@ -1,7 +1,7 @@
 const StakeCapitalTemplate = artifacts.require("StakeCapitalTemplate")
 
-const STAKE_CAPITAL_TEMPLATE_ADDRESS = "0xa5abc5931cca45040e4e0a077b10ffe52d7b98dc"
-const STAKE_CAPITAL_DAO_ID = "stake-capital-b" // This ID must be unique, change it for each new deployment or a revert will occur
+const STAKE_CAPITAL_TEMPLATE_ADDRESS = "0x35e415382647ea72372eb02190c968eaa119a973"
+const STAKE_CAPITAL_DAO_ID = "stake-capital-e" // This ID must be unique, change it for each new deployment or a revert will occur
 
 module.exports = async () => {
     try {
@@ -9,7 +9,7 @@ module.exports = async () => {
         let template = await StakeCapitalTemplate.at(STAKE_CAPITAL_TEMPLATE_ADDRESS)
 
         console.log(`Creating DAO...`)
-        let receipt = await template.newTokenAndInstance("Test", "TST", STAKE_CAPITAL_DAO_ID, ["0xb4124ceb3451635dacedd11767f004d8a28c6ee7"], ["1000000000000000000"], ["10000000000000000", "10000000000000000", "3000"], 0, true)
+        let receipt = await template.newTokenAndInstance("Test", "TST", STAKE_CAPITAL_DAO_ID, ["0xb4124ceb3451635dacedd11767f004d8a28c6ee7"], ["1000000000000000000"], ["10000000000000000", "10000000000000000", "3000"], true)
 
         console.log(`New DAO created at: ${receipt.logs.find(x => x.event === "DeployDao").args.dao}\n`)
 
